14afeede9e4cc626ebf2756f4a638ef2ada1b0ec
Rostercen Einstien Pulido <aerostien2@gmail.com>
2025-05-26 03:45:45 +0800

    fix reference error

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('../controllers/product');
+
+const {
+  createProduct,
+  getAllProducts,
+  getActiveProducts,
+  getProductById,
+  updateProduct,
+  archiveProduct,
+  activateProduct
+} = require('../controllers/product');
 
 const authenticate = require('../middleware/authenticate');
 
