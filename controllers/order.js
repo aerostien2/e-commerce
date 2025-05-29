fc1ced4e0edfe611f7cd7aa4fdc0036e132fe023
jbsartedavid <jbsartedavid@gmail.com>
2025-05-29 21:07:14 +0800

    fixing

--- controllers/order.js
+++ controllers/order.js
@@ -42,7 +42,7 @@ exports.allOrders = async(req, res) => {
 	try{
 
 	   const orders = await Order.find();
-	   return res.status(200).json({ orders: orders });
+	   res.status(200).json({ orders: orders });
 
 	} catch (err) {
     return res.status(500).json({ error: err.message });
